fc3b7b0c59cb2f097194fb21a1577d08ebd23a17
Rafael Mileris <rafaelmileris15@gmail.com>
2022-12-22 15:20:02 -0300

    Pasta do formulario e do banco do formulario

---
+++ Formulario/formulario.py
@@ -0,0 +1,18 @@
+import os
+import banco
+
+"""
+Formulário criado para manipular as informações de um banco de dados
+"""
+
+print("Selecione uma opcao\n")
+
+#Criar uma nova tabela
+print("1 - Criar nova tabela")
+nome_tb = input("Nome da tabela: ")
+col1 = input("Nome da coluna 1:")
+col2 = input("Nome da coluna 2:")
+col3 = input("Nome da coluna 3:")
+col4 = input("Nome da coluna 4:")
+col5 = input("Nome da coluna 5:")
+os.system('pause')
