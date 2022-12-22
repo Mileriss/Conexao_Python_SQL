0bdfc533e7bb7721f7158096355ed6b4db6ec349
Rafael Mileris <rafaelmileris15@gmail.com>
2022-12-22 15:35:52 -0300

    Deixando tudo em um arquivo unico

--- Formulario/formulario.py
+++ Formulario/formulario.py
@@ -1,18 +1,52 @@
 import os
-import banco
+import sqlite3
+from sqlite3 import Error
 
+print("Formulario para manipular banco de dados\n\n")
+
+#Função para conectar ao banco de dados 'formulario'
+def conexaoBanco():
+    caminho = "C:\\Users\\s5057981\\OneDrive\\Documentos\\SQLite\\formulario.db"
+    conexao = None
+    try:
+        conexao = sqlite3.connect(caminho)
+    except Error as excecao:
+        print(excecao)
+    return conexao
+
+var_conexao = conexaoBanco()
+
+
+#----------Criar uma nova tabela----------
+try:
+    print("1 - Criar nova tabela")
+    nome_tb = input("Nome da tabela: ")
+    col1 = input("Nome da coluna 1:")
+    col2 = input("Nome da coluna 2:")
+    col3 = input("Nome da coluna 3:")
+    col4 = input("Nome da coluna 4:")
+    col5 = input("Nome da coluna 5:")
+except Error as excecao:
+    print(excecao)
+
+cols_tb = f"""
+CREATE TABLE {nome_tb}(
+    ID integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    {col1} VARCHAR(50) NOT NULL,
+    {col2} VARCHAR(50) NOT NULL,
+    {col3} INTEGER(5) NOT NULL,
+    {col4} INTEGER(20) NOT NULL,
+    {col5} VARCHAR(30) NOT NULL
+)
 """
-Formulário criado para manipular as informações de um banco de dados
-"""
+def criarTB(conexao,query_sql):
+    
+    try:
+        conectar = conexao.cursor()
+        conectar.execute(query_sql)
+    except Error as excecao:
+        print(excecao)
 
-print("Selecione uma opcao\n")
+criarTB(var_conexao, cols_tb)
 
-#Criar uma nova tabela
-print("1 - Criar nova tabela")
-nome_tb = input("Nome da tabela: ")
-col1 = input("Nome da coluna 1:")
-col2 = input("Nome da coluna 2:")
-col3 = input("Nome da coluna 3:")
-col4 = input("Nome da coluna 4:")
-col5 = input("Nome da coluna 5:")
-os.system('pause')
+var_conexao.close()
